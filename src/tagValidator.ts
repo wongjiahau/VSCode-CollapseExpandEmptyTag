0ae8a2ef2abdc0b859e96229ddf5198a71758d08
WJH <hou32hou@gmail.com>
2017-10-03 17:30:04 +0800

    Passed test for IsValidTag of TagValidator

--- src/tagValidator.ts
+++ src/tagValidator.ts
@@ -1,11 +1,11 @@
 import * as helper from './helper'
+import * as tagExtractor from './tagExtractor'
 
 export function IsValidTag(str: string){
     if(helper.CountChar(str, "<") < 2) return false;
     if(helper.CountChar(str, ">") < 2) return false;
     if(helper.CountChar(str, "/") < 1) return false;
-    
-
+    if(tagExtractor.GetOpenTagValue(str)!=tagExtractor.GetCloseTagValue(str)) return false;
 }
 
 export function IsEmptyTag(tag: string){
