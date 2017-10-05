b112420861d8a846ee0fc896da60776d52e91dfd
WJH <hou32hou@gmail.com>
2017-10-05 16:14:39 +0800

    Added check for void tag test for jsxParser

--- src/test/jsxParser.test.ts
+++ src/test/jsxParser.test.ts
@@ -1,3 +1,6 @@
+import {
+    at
+} from 'lodash';
 //
 // Note: This example test is leveraging the Mocha test framework.
 // Please refer to their documentation on https://mochajs.org/ for help.
@@ -9,9 +12,6 @@ import * as assert from 'assert';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-import {
-    Reform
-} from '../jsxTreeReformer'
 import JSXParser from 'jsx-parser'
 
 import {
@@ -22,6 +22,18 @@ suite("JSXParser Tests", () => {
     test("Jsonify 1", () => {
         var str = `<Button onClick={()=>{ /*some action*/}} height="123" width=' '/>`
         console.log(JSON.stringify(JSXParser(str), null, " "))
+    });
+
+    test("Check for void tag 1", () => {
+        var tag = `<vege></vege>`
+        var tree = JSXParser(tag);
+        assert.equal( at(tree, 'isVoidTag')[0], undefined);
+    });
+
+    test("Check for void tag 2", () => {
+        var tag = `<vege/>`
+        var tree = JSXParser(tag);
+        assert.equal( at(tree, 'isVoidTag')[0], true);
     });
 
 });
